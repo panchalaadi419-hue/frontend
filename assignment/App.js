1784f0f01600fc270128e6b40e9adfd541e75648
panchalaadi419-hue <panchalaadi419@gmail.com>
2025-08-14 04:41:37 +0530

    Create App.js

---
+++ assignment/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+// import Hello from "./hello/Hello";
+// import Jsxelem from "./hello/Jsxelem";
+// import Greeting from "./hello/Greeting";
+// import Welcome from "./hello/Welcome";
+// import Usercard from "./hello/Usercard";
+import Counter from "./hello/Counter";
+
+
+
+
+
+function App() {
+    return (
+        <div className="App">
+
+            {/* <Hello/> */}
+            {/* <Jsxelem/> */}
+            {/* <Greeting name="vivek"/> */}
+            {/* <Welcome/> */}
+            {/* <Usercard name="vivek" age="21" location="ahmedabad"/> */}
+            <Counter/>
+
+
+
+        </div>
+
+    )
+}
+
+export default App;
