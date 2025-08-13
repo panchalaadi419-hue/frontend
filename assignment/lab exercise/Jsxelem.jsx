f6ad1dc534d145f7bd6a6485835be8a75fdc48e9
panchalaadi419-hue <panchalaadi419@gmail.com>
2025-08-14 04:36:59 +0530

    Create Jsxelem.jsx

---
+++ assignment/lab exercise/Jsxelem.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Jsxelem() {
+
+         var topic="jsx (javascript xml)";
+         var description="a paragraph explain jsx with dynamic data";
+
+
+
+
+  return (
+
+    <div>
+        <h1>welcome to jsx</h1>
+        <p>{topic } jsx allow you to embed javascript expressions directly within the html like syntax </p>
+    </div>
+    
+   
+
+
+  )
+}
+
+export default Jsxelem
